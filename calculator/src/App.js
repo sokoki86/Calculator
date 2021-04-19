59ba5294346a7591471dd4a324832c4d24d90b4d
Jessica <jmbaskerville1217@gmail.com>
2021-04-19 14:22:17 -0400

    added properties to buttons and styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,59 +1,66 @@
-import React, {Component} from 'react';
-import './App.css';
-import Button from './components/buttons.js'
+import React, { Component } from "react";
+import "./App.css";
+import Button from "./components/buttons.js";
+import "./css/style.css";
 
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      current: '',
+      current: "0",
       previous: []
-    }
+    };
   }
 
   reset = () => {
-  this.setState({result: '0'});
-}
+    this.setState({ result: '0' });
+  };
 
-addToCurrent = (symbol) => {
-  this.setState({current: this.state.current + symbol});
-}
-
+  addToCurrent = (symbol) => {
+    console.log("symbol");
+    this.setState({ current: this.state.current + symbol });
+  };
 
   render() {
-  const buttons = [
-    {symbol: 'C', cols: 3, action: this.reset},
-    {symbol: '/', cols: 1, action: this.addToCurrent},
-    {symbol: '7', cols: 1, action: this.addToCurrent},
-    {symbol: '8', cols: 1, action: this.addToCurrent},
-    {symbol: '9', cols: 1, action: this.addToCurrent},
-    {symbol: 'X', cols: 1, action: this.addToCurrent},
-    {symbol: '4', cols: 1, action: this.addToCurrent},
-    {symbol: '5', cols: 1, action: this.addToCurrent},
-    {symbol: '6', cols: 1, action: this.addToCurrent},
-    {symbol: '-', cols: 1, action: this.addToCurrent},
-    {symbol: '1', cols: 1, action: this.addToCurrent},
-    {symbol: '2', cols: 1, action: this.addToCurrent},
-    {symbol: '3', cols: 1, action: this.addToCurrent},
-    {symbol: '+', cols: 1, action: this.addToCurrent},
-    {symbol: '0', cols: 2, action: this.addToCurrent},
-    {symbol: '.', cols: 1, action: this.addToCurrent},
-    {symbol: '=', cols: 1, action: this.addToCurrent},
-  ];
+    const buttons = [
+      { symbol: "C", cols: 3, action: this.reset },
+      { symbol: "/", cols: 1, action: this.addToCurrent },
+      { symbol: "7", cols: 1, action: this.addToCurrent },
+      { symbol: "8", cols: 1, action: this.addToCurrent },
+      { symbol: "9", cols: 1, action: this.addToCurrent },
+      { symbol: "X", cols: 1, action: this.addToCurrent },
+      { symbol: "4", cols: 1, action: this.addToCurrent },
+      { symbol: "5", cols: 1, action: this.addToCurrent },
+      { symbol: "6", cols: 1, action: this.addToCurrent },
+      { symbol: "-", cols: 1, action: this.addToCurrent },
+      { symbol: "1", cols: 1, action: this.addToCurrent },
+      { symbol: "2", cols: 1, action: this.addToCurrent },
+      { symbol: "3", cols: 1, action: this.addToCurrent },
+      { symbol: "+", cols: 1, action: this.addToCurrent },
+      { symbol: "0", cols: 2, action: this.addToCurrent },
+      { symbol: ".", cols: 1, action: this.addToCurrent },
+      { symbol: "=", cols: 1, action: this.addToCurrent },
+    ];
 
-  return (
-    <div className="App">
-      <input className="result" type="text" value={this.state.curent} />
-      
-      {buttons.map((btn, i) => {
-        return <Button symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action}/>      })}
-    
-    
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <input className="result" type="text" value={this.state.current} />
+
+        {buttons.map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              symbol={btn.symbol}
+              cols={btn.cols}
+              action={(symbol) => btn.action(symbol)}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- calculator/src/components/buttons.js
+++ calculator/src/components/buttons.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 class Button extends Component {
-    render(){
-        return(
-            <div>
-                <button class="calc-button">1</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={`column-${this.props.cols}`}>
+        <button
+          className="calc-button"
+          onClick={() => this.props.action(this.props.symbol)}
+        >
+          {this.props.symbol}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Button
+export default Button;
